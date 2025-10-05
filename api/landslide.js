12b97392f0d2936dbb213dc3dcdff649e3387596
Gokul <gokulpjayan2004@gmail.com>
2025-10-05 10:58:39 +0530

    Add logging for NASA API response status in landslide data handler

--- api/landslide.js
+++ api/landslide.js
@@ -4,6 +4,7 @@ export default async function handler(req, res) {
       "https://maps.nccs.nasa.gov/server/rest/services/global_landslide_catalog/landslide_susceptibility/MapServer/query?where=1%3D1&outFields=*&f=geojson";
 
     const response = await fetch(mapServerUrl);
+    console.log("NASA response status:", response.status);
     if (!response.ok) {
       throw new Error(`Susceptibility API error: ${response.status}`);
     }
